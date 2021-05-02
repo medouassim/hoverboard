821477ce8686b8037e75665ad94797e968b0e544
medouassim <55862605+medouassim@users.noreply.github.com>
2021-05-02 14:55:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ for(var i =0; i < squares; i++) {
         e.target.style.backgroundColor = `#${color}`
         e.target.style.boxShadow = `0 0 2px #${color}, 0 0 10px #${color}`
     })
+     square.addEventListener("touchstart", (e) => {
+        var color = colors[Math.floor(Math.random() * colors.length)];
+        e.target.style.backgroundColor = `#${color}`
+        e.target.style.boxShadow = `0 0 2px #${color}, 0 0 10px #${color}`
+    })
     square.addEventListener('mouseleave', (e) => {
+        
+            e.target.style.backgroundColor = "#1d1d1d";
+            e.target.style.boxShadow = '0 0 1px #fff';
+    })
+    square.addEventListener('touchcancel', (e) => {
         
             e.target.style.backgroundColor = "#1d1d1d";
             e.target.style.boxShadow = '0 0 1px #fff';
